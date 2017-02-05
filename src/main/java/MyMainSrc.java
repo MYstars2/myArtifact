627d3b24a217739843ca3001eaf873ce7758cc25
JasonMsh <lemonade98@gmail.com>
2017-02-05 22:34:55 +0900

    first commit!

---
+++ src/main/java/MyMainSrc.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class MyMainSrc {
+
+	private void aMethod(int i) {
+		System.out.println("a-Method. i=" + i);
+	}
+
+	private void bMethod() {
+		System.out.println("b-Method-ccc-ddd---stash test");
+
+	}
+}
---
+++ src/test/java/MyTestFile.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTestFile {
+
+	@Test
+	public void test() {
+		assertTrue("1", "1".equals("1"));
+
+	}
+}
